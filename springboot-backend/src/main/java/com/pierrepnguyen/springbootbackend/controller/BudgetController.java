3475ee4750dd514d77210a6e86b8b0cd5625726b
pierrepnguyen <pierrepnguyen@gmail.com>
2023-01-18 13:30:31 -0500

    get all controller

---
+++ springboot-backend/src/main/java/com/pierrepnguyen/springbootbackend/controller/BudgetController.java
@@ -0,0 +1,27 @@
+package com.pierrepnguyen.springbootbackend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pierrepnguyen.springbootbackend.model.Budget;
+import com.pierrepnguyen.springbootbackend.repository.BudgetRepository;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/")
+public class BudgetController {
+  
+  @Autowired
+  private BudgetRepository budgetRepository;
+
+  // get all
+  @GetMapping("/view-all")
+  public List<Budget> getAllBudgets(){
+    return budgetRepository.findAll();
+  }
+}
