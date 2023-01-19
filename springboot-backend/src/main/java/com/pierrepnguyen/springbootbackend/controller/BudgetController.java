37faf66f1d2f99ede791174088fbe780aafe705d
pierrepnguyen <pierrepnguyen@gmail.com>
2023-01-19 15:01:39 -0500

    create budget api endpoint

--- springboot-backend/src/main/java/com/pierrepnguyen/springbootbackend/controller/BudgetController.java
+++ springboot-backend/src/main/java/com/pierrepnguyen/springbootbackend/controller/BudgetController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,11 @@ public class BudgetController {
   @GetMapping("/view-all")
   public List<Budget> getAllBudgets(){
     return budgetRepository.findAll();
+  }
+
+  // create employee rest api
+  @PostMapping("/view-all")
+  public Budget createBudget(@RequestBody Budget budget){
+    return budgetRepository.save(budget);
   }
 }
